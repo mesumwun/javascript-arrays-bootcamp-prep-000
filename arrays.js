c728b6f53409e93dab07ccb95509d8a9a09be4e3
Daniel Hope <upinhope@gmail.com>
2019-03-06 23:40:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1);
+  array.slice(1);
+  return array;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
